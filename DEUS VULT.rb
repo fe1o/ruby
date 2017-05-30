d7ef1f37fa5b2dba46758a0270c62a61836fdbd2
t4rkus <luizfcsp80@gmail.com>
2017-05-29 21:49:06 -0300

    Add files via upload

---
+++ DEUS VULT.rb
@@ -0,0 +1,15 @@
+def dv
+	print "----------------------------------- \n"
+	print " You Deus Vult my man? say 'true' or 'false'\n"
+	print "----------------------------------- \n"
+
+	vult = gets.chomp
+
+	if vult == "true"
+		print "DEUS VULT my man \n"
+	else 
+		print "DIE FILTHY SINNER \n"
+	end
+end
+
+dv
