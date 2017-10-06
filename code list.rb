ba2ce0069d058f4b5cad70ac2893864520a5882f
Luiz Fernando Cavalcante <luizfcsp80@gmail.com>
2017-10-06 09:48:52 -0300

    Update code list.rb

--- code list.rb
+++ code list.rb
@@ -202,3 +202,29 @@ puts user.name
 puts user.email
 user.walk("stroling")
 =end
+=begin
+def alphabetize(arr, rev=false)
+  if rev
+    arr.sort { |item1, item2| item2 <=> item1 }
+  else
+    arr.sort { |item1, item2| item1 <=> item2 }
+  end
+end
+
+books = ["Heart of Darkness", "Code Complete", "The Lorax", "The Prophet", "Absalom, Absalom!"]
+
+puts "A-Z: #{alphabetize(books)}"
+puts "Z-A: #{alphabetize(books, true)}"
+=end
+=begin
+def alphabetize(arr, rev=false)	
+	arr.sort!
+  if rev == true
+    arr.reverse!
+  else
+    return arr
+  end
+end
+numbers = [4,2,8,2,4,1,6,5,7]
+puts alphabetize(numbers)
+=end
