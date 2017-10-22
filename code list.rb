cf1ed283edae7938d2c0c323faf1bedc13764c18
Luiz Fernando Cavalcante <luizfcsp80@gmail.com>
2017-10-22 10:09:31 -0300

    Update code list.rb

--- code list.rb
+++ code list.rb
@@ -339,3 +339,18 @@ case choice
   	puts "Error!"
 end
 =end
+=begin
+# ThePresent has a .now method that we'll extend to TheHereAnd
+
+module ThePresent
+  def now
+    puts "It's #{Time.new.hour > 12 ? Time.new.hour - 12 : Time.new.hour}:#{Time.new.min} #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
+  end
+end
+
+class TheHereAnd
+  extend ThePresent
+end
+
+TheHereAnd.now
+=end
